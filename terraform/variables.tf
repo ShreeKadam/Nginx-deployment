e7929778177748845c62d877f55d481d85814d34
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 22:47:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,25 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
+variable "region" {
+  description = "AWS region to use"
   type        = string
-  default     = "us-east-1" # or whatever region you need
 }
 
-
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Name of the AWS key pair"
+  description = "Key pair name to SSH into EC2 instances"
   type        = string
-  default     = "deployer-key"
-}
-
-variable "ami" {
-  description = "AMI ID for instances"
-  type        = string
-  default     = "ami-0c02fb55956c7d316"  # update as needed
 }
