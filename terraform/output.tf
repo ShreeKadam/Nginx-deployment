3755a9118fc97ce63d9b7ce3e8afc63a5ac89863
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 18:50:45 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -10,3 +10,15 @@ output "bastion_public_ip" {
 output "private_instance_ip" {
   value = aws_instance.private_ec2.private_ip
 }
+
+output "private_key" {
+  description = "Private key PEM for SSH access"
+  value       = tls_private_key.example.private_key_pem
+  sensitive   = true
+}
+
+output "key_name" {
+  description = "Key pair name"
+  value       = aws_key_pair.deployer.key_name
+}
+
