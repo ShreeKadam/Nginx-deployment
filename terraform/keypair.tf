dd61bfcc29ecd57465b728636924525fe483dbc6
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 19:23:12 +0530

    Update keypair.tf

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -9,13 +9,13 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "local_file" "private_key" {
-  filename        = "${path.module}/private_key.pem"
+  filename        = "${path.module}/deployer.pem"    # <-- original pem file name here
   content         = tls_private_key.generated.private_key_pem
   file_permission = "0600"
 }
 
 resource "local_file" "public_key" {
-  filename        = "${path.module}/public_key.pem"
+  filename        = "${path.module}/deployer.pub"
   content         = tls_private_key.generated.public_key_openssh
   file_permission = "0644"
 }
