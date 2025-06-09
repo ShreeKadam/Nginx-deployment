31bad48e3c8d6d0c72714a2148b1bff251281e97
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 22:48:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,11 +93,4 @@ resource "aws_instance" "nginx_private" {
   }
 }
 
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
-}
-
-output "private_instance_ip" {
-  value = aws_instance.nginx_private.private_ip
-}
 
