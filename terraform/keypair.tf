b176d3c42a184077553a541307513d3e55c202aa
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 19:31:16 +0530

    Update keypair.tf

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,24 +1,24 @@
-#resource "tls_private_key" "generated" {
- # algorithm = "RSA"
-  #rsa_bits  = 4096
-#}
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-#resource "aws_key_pair" "deployer" {
-#  key_name   = "deployer-key"
-#  public_key = tls_private_key.generated.public_key_openssh
-#}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.generated.public_key_openssh
+}
 
-#resource "local_file" "private_key" {
-#  filename        = "${path.module}/deployer.pem"    # <-- original pem file name here
-#  content         = tls_private_key.generated.private_key_pem
-#  file_permission = "0600"
-#}
+resource "local_file" "private_key" {
+  filename        = "${path.module}/deployer.pem"    # <-- original pem file name here
+  content         = tls_private_key.generated.private_key_pem
+  file_permission = "0600"
+}
 
-#resource "local_file" "public_key" {
-#  filename        = "${path.module}/deployer.pub"
-#  content         = tls_private_key.generated.public_key_openssh
-#  file_permission = "0644"
-#}
+resource "local_file" "public_key" {
+  filename        = "${path.module}/deployer.pub"
+  content         = tls_private_key.generated.public_key_openssh
+  file_permission = "0644"
+}
 # Generate a private key (for ssh)
 resource "tls_private_key" "example" {
   algorithm = "RSA"
