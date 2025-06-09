73ac35667f3178869782f592ad78080f0e8d3cf9
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 22:07:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,9 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1" # or whatever region you need
 }
+
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
