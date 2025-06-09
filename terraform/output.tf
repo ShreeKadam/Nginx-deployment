f28aed3ac7d032a26206ac41a7fc729663796d7d
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 17:45:39 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,9 @@
-# ----------------------
-# Outputs
-# ----------------------
 output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
+  description = "Public IP of bastion host"
+  value       = aws_instance.bastion.public_ip
 }
 
-output "nginx_private_ip" {
-  value = aws_instance.nginx_private.private_ip
+output "private_instance_private_ip" {
+  description = "Private IP of private NGINX instance"
+  value       = aws_instance.private_nginx.private_ip
 }
