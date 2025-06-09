70f2c87f39399557fad8da38dbb148cdadc2a230
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 18:51:12 +0530

    Create keypair.tf

---
+++ terraform/keypair.tf
@@ -0,0 +1,9 @@
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
