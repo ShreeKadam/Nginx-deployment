c2d42d99101503d07abb1495da990db561eb1f71
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 22:43:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,21 @@
 #  region = var.region
 #}
 
-resource "tls_private_key" "this" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
+#resource "tls_private_key" "this" {
+#  algorithm = "RSA"
+#  rsa_bits  = 2048
+#}
 
 #resource "aws_key_pair" "deployer" {
 #  key_name   = var.key_name
 #  public_key = tls_private_key.this.public_key_openssh
 #}
 
-resource "local_file" "pem_key" {
-  filename = "${path.module}/../keys/${var.key_name}.pem"
-  content  = tls_private_key.this.private_key_pem
-  file_permission = "0600"
-}
+#resource "local_file" "pem_key" {
+#  filename = "${path.module}/../keys/${var.key_name}.pem"
+#  content  = tls_private_key.this.private_key_pem
+#  file_permission = "0600"
+#}
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
