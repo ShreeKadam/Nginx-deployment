65d9066063bd8eeb22cdc6174b354bff5245ac16
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-09 21:46:38 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region" {
+variable "region" {
   default = "us-east-1"
 }
 
@@ -6,25 +6,10 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
-}
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-variable "ami_id" {
-  description = "Amazon Linux 2 AMI"
-  default     = "ami-0c02fb55956c7d316" # Update for region if needed
-}
-
 variable "key_name" {
-  default = "auto-key"
+  default = "shared-key"
+}
+
+variable "ami" {
+  default = "ami-0c02fb55956c7d316" # Amazon Linux 2
 }
