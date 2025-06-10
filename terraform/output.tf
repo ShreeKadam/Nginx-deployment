fa7747ac1ef155f8916bd7771a7fed46ca651892
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-10 12:19:08 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,23 @@ output "bastion_public_ip" {
 output "private_instance_ip" {
   value = aws_instance.nginx_private.private_ip
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.nginx_alb.dns_name
+}
