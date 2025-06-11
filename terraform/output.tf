5236d3ac560d383bcf65654c93ec154e732559a2
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-11 22:07:03 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,14 @@ output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
 
+output "private_instance_ids" {
+  value = aws_instance.nginx_private[*].id
+}
+
+output "private_instance_private_ips" {
+  value = aws_instance.nginx_private[*].private_ip
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
