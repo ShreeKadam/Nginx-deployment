e243bf5b21c46b22aed7f4143e1c1e7675d92346
ShreeKadam <105924765+ShreeKadam@users.noreply.github.com>
2025-06-29 00:18:19 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,16 @@
+terraform {Add commentMore actions
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
 provider "aws" {
   region = var.region
 }
